29aed0028d2c524485b05a87664725483c48a9f1
Tejeda009 <114180240+Tejeda009@users.noreply.github.com>
2025-12-25 11:00:21 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: 'friendly-fix-spark',
   server: {
     host: "::",
     port: 8080,
